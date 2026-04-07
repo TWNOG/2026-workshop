23719f9d9022036a2568060d34c5429ecb8b717f
OsGa <oscarhuang950324@gmail.com>
2026-04-07 10:07:01 +0800

    feat: add wave hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    defaultLocale: 'zh',
+    locales: ['zh', 'en'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+});
